9f32bb4888b73dc87d7d1be59ff5dfb2cc85bb3c
gchild320 <gchild320@gmail.com>
2014-03-09 10:55:26 -0500

    wtf t0ltetmo ghost

--- slim.mk
+++ slim.mk
@@ -1,8 +1,8 @@
 # Specify phone tech before including full_phone
-$(call inherit-product, vendor/slim/config/gsm.mk)
+$(call inherit-product, vendor/slim/config/cdma.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := t0ltetmo
+PRODUCT_RELEASE_NAME := i605
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
@@ -12,17 +12,17 @@ TARGET_SCREEN_WIDTH := 720
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/t0ltetmo/full_t0ltetmo.mk)
+$(call inherit-product, device/samsung/t0ltetmo/full_i605.mk)
 
 # Inherit torch settings
 $(call inherit-product, vendor/slim/config/common_ledflash.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := t0ltetmo
-PRODUCT_NAME := slim_t0ltetmo
+PRODUCT_DEVICE := i605
+PRODUCT_NAME := slim_i605
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SGH-T889
+PRODUCT_MODEL := SCH-I605
 PRODUCT_MANUFACTURER := samsung
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=t0ltetmo TARGET_DEVICE=t0ltetmo BUILD_FINGERPRINT="samsung/t0ltetmo/t0ltetmo:4.1.2/JZO54K/T889UVBMB4:user/release-keys" PRIVATE_BUILD_DESC="t0ltetmo-user 4.1.2 JZO54K T889UVBMB4 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=t0ltevzw TARGET_DEVICE=t0ltevzw BUILD_FINGERPRINT="Verizon/t0ltevzw/t0ltevzw:4.3/JSS15J/I605VRUEMJ9:user/release-keys" PRIVATE_BUILD_DESC="t0ltevzw-user 4.3 JSS15J I605VRUEMJ9 release-keys"
